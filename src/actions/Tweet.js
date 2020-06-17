185991d877798e254b81dff9f9b46f1ba557924e
Shu <shu.liang89@gmail.com>
2020-06-16 20:56:06 -0400

    updated frontend with backend api

--- src/actions/Tweet.js
+++ src/actions/Tweet.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getTweetSentiment = async (url) => {
     try {
-        let result = await axios.get('https://reqres.in/api/users/2');
+        let result = await axios.get('https://sat-backend.herokuapp.com/api/v1/sentiment_analyses/12312312321');
         return result.data; 
     } catch (err) {
         console.log(err.response);
