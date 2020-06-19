19539c116c32bdd375d7dcf3d958675a2b797521
Nicole <shudvprojects@gmail.com>
2020-06-18 22:46:06 -0400

    Revert "updated background"

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -25,15 +25,12 @@ class Landing extends Component {
     render(){
         return (
             <div className="fill">
-                <section className="bg-content fill">
-                </section>
-                <div className="main-content"> 
-                    <h1 className="appTitle">Sentiment Analysis</h1>
-                    <div className="contentBody">
-                        <InputBox saveTweetUrlToLanding={this.setTweetUrl.bind(this)} saveSentimentToLanding={this.setResult.bind(this)}/>
-                        <SentimentOutput sentiment={this.state.result}/>
-                    </div>
-                </div> 
+                <h1 className="appTitle">Sentiment Analysis</h1>
+                <div className="contentBody">
+                    <InputBox saveTweetUrlToLanding={this.setTweetUrl.bind(this)} saveSentimentToLanding={this.setResult.bind(this)}/>
+                    <SentimentOutput sentiment={this.state.result}/>
+                </div>
+                
             </div>
         );
     }
