7c6827b2b40288daac095cf33b744f449a737c25
Shu <shu.liang89@gmail.com>
2020-07-03 09:48:03 -0400

    updated results

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -13,7 +13,7 @@ class Landing extends Component {
         super(props);
         this.state = {
             currentTweetUrl: "",
-            result: ["waiting for input"]
+            result: []
         };
     };
 
